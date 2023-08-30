f258ce34b147b0e15fb9d175b032c181f5644752
Kadiraydn01 <kadiraydn01@gmail.com>
2023-08-31 01:26:01 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import { useState } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Movie from "./components/Movie";
 import FavMovie from "./components/FavMovie";
+import { useDispatch, useSelector } from "react-redux";
+import { addList, nextMovie, prevMovie, removeList } from "./actions/action";
 
 function App() {
-  const [sira, setSira] = useState(0);
-  const favMovies = [];
+  const favMovies = useSelector((store) => store.favMovies);
+  const movies = useSelector((store) => store.movies);
+  const sira = useSelector((store) => store.sira);
+  const dispatch = useDispatch();
 
-  function sonrakiFilm() {
-    setSira(sira + 1);
-  }
+  const handleAddList = () => {
+    dispatch(addList());
+  };
 
+  const handleNextMovie = () => {
+    dispatch(nextMovie());
+  };
+
+  const handlePrevMovie = () => {
+    dispatch(prevMovie());
+  };
+
+  const handleRemoveList = () => {
+    dispatch(removeList(movies[sira].id));
+  };
   return (
     <div className="wrapper max-w-2xl mx-auto">
       <nav className="flex text-2xl pb-6 pt-8 gap-2 justify-center">
-        <NavLink to="/" exact className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/"
+          exact
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Filmler
         </NavLink>
-        <NavLink to="/listem" className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/listem"
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Listem
         </NavLink>
       </nav>
@@ -27,13 +50,28 @@ function App() {
 
           <div className="flex gap-3 justify-end py-3">
             <button
-              onClick={sonrakiFilm}
+              onClick={handlePrevMovie}
+              className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+            >
+              Önceki
+            </button>
+            <button
+              onClick={handleNextMovie}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
               Sıradaki
             </button>
-            <button className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white">
+            <button
+              onClick={handleAddList}
+              className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white"
+            >
               Listeme ekle
+            </button>
+            <button
+              onClick={handleRemoveList}
+              className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white"
+            >
+              Listemden Çıkar
             </button>
           </div>
         </Route>
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,7 +1,46 @@
-import React from "react";
+import {
+  ADD_LIST,
+  NEXT_MOVIE,
+  PREV_MOVIE,
+  REMOVE_LIST,
+} from "../actions/action";
+import { movies } from "../movies";
 
-const reducer = () => {
-  return <div>reducer</div>;
+const initialState = {
+  movies: movies,
+  favMovies: [],
+  sira: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_LIST:
+      const movie = state.movies[state.sira];
+      return {
+        ...state,
+        sira: state.sira + 1,
+        favMovies: [...state.favMovies, movie],
+      };
+    case NEXT_MOVIE:
+      return {
+        ...state,
+        sira: state.sira + 1,
+      };
+    case PREV_MOVIE:
+      return {
+        ...state,
+        sira: state.sira - 1,
+      };
+    case REMOVE_LIST:
+      return {
+        ...state,
+        favMovies: state.favMovies.filter(
+          (movie) => movie.id !== action.payload
+        ),
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
